d9e19b8ebfe6f9b7fa88f4894209d5ebc6b29d16
nsriv531 <nsriv531@mtroyal.ca>
2024-10-03 20:34:36 -0600

    sdfsd

--- lab12b/lab12b-test02.php
+++ lab12b/lab12b-test02.php
@@ -1,61 +1,55 @@
 <?php
+// Enable error reporting to catch any hidden issues
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-include('includes/lab12b-test01.inc.php');
+// Check if the file parameter is set and valid
+$fileIndex = isset($_GET['file']) && is_numeric($_GET['file']) ? (int)$_GET['file'] : 0;
+$imagePath = "images/art/{$fileIndex}.jpg";
 
-// Function to output a single day forecast
-function outputDayForecast($day, $forecast) {
-    echo "<div>";
-    echo "<h3>{$day}</h3>";
-    echo "<p><i class='wi wi-day-{$forecast[1]}'></i></p>";
-    echo "<p>{$forecast[0]}</p>";
-    echo "</div>";
+// Debug: Check if the image path is correct (comment this out after testing)
+// echo $imagePath;
+
+// Check if the image exists
+if (!file_exists($imagePath)) {
+    die("Error: Image file not found at $imagePath");
 }
 
-// Function to output a single city box
-function outputCityBox($city, $data) {
-    echo "<article class='box'>"; 
-    echo "<h1>{$city}</h1>";
-    
-    // Current weather
-    echo "<div class='weather'>";
-    echo "<img src='images/{$city}.jpg' />";
-    echo "<div>";
-    echo "<h2>{$data[0]}</h2>";
-    echo "<p>" . ucfirst($data[1]) . "</p>";                
-    echo "</div>";
-    echo "</div>";
-    
-    // 5-day forecast
-    echo "<section>";
-    foreach ($data[2] as $day => $forecast) {
-        outputDayForecast($day, $forecast);
-    }
-    echo "</section>";
-    
-    echo "</article>";
+// Load the image
+$image = imagecreatefromjpeg($imagePath);
+if (!$image) {
+    die("Error: Could not load image.");
 }
 
+// Check if the texts and font sizes are set
+$memeText1 = isset($_GET['text1']) ? $_GET['text1'] : '';
+$memeSize1 = isset($_GET['size1']) && is_numeric($_GET['size1']) ? (int)$_GET['size1'] : 24;
+
+$memeText2 = isset($_GET['text2']) ? $_GET['text2'] : '';
+$memeSize2 = isset($_GET['size2']) && is_numeric($_GET['size2']) ? (int)$_GET['size2'] : 24;
+
+// Check if the width parameter is set and resize the image
+$width = isset($_GET['width']) && is_numeric($_GET['width']) ? (int)$_GET['width'] : null;
+if ($width) {
+    $image = imagescale($image, $width, $width);  // Keeping the height the same as width
+}
+
+// Set text color and font file (ensure the font file path is correct)
+$black = imagecolorallocate($image, 0, 0, 0);
+$font = 'font/Lato-Heavy.ttf'; // Make sure you have a valid .ttf font file here
+
+// Add the first meme text
+if (!empty($memeText1)) {
+    imagettftext($image, $memeSize1, 0, 20, 50, $black, $font, $memeText1); // Position: 20px from left, 50px from top
+}
+
+// Add the second meme text
+if (!empty($memeText2)) {
+    imagettftext($image, $memeSize2, 0, 20, imagesy($image) - 50, $black, $font, $memeText2); // Bottom of the image
+}
+
+// Output the image to the browser
+header('Content-Type: image/jpeg');
+imagejpeg($image);
+imagedestroy($image);
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>  
-    <title>Lab 12b</title>   
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">   
-    <link rel="stylesheet" href="css/weather-icons.min.css">
-    <link rel="stylesheet" href="css/lab12b-test01.css">
-</head>
-<body>
-<main class="grid-container">
-
-    <?php
-    // Loop through the weather data and output city boxes
-    foreach ($weatherData as $city => $data) {
-        outputCityBox($city, $data);
-    }
-    ?>
-
-</main>   
-</body>
-</html>
